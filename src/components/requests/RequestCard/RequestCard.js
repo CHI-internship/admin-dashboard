132e4edba85ce595fdd6ae6c89c10aec24c66bb4
TanyaDovzhenko <tanyadovzzhenko@gmail.com>
2022-12-04 21:46:14 +0200

    fix request card

---
+++ src/components/requests/RequestCard/RequestCard.js
@@ -0,0 +1,23 @@
+import style from './RequestCard.module.scss'
+import arrowIcon from '../../../images/arrow.svg'
+import { Link } from 'react-router-dom';
+
+
+export const RequestCard = (request) => {
+
+    return (
+        <div className={style.card}>
+            <div className={style.info}>
+                <div className={style.infoItem}>Country: {request.request.country}</div>
+                <div className={style.infoItem}>Card number: {request.request.card_number}</div>
+            </div>
+            <Link
+                className={style.arrowContainer}
+                to={`/admin/request/${request.request.id}`}
+                state={request.request.id}
+            >
+                <img src={arrowIcon} className={style.arrow} />
+            </Link>
+        </div>
+    )
+}
