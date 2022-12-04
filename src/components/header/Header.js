37249839661469e4e2b85a53a090c36289d8c09d
TanyaDovzhenko <tanyadovzzhenko@gmail.com>
2022-12-04 21:46:54 +0200

    add header menu items

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,22 +1,59 @@
-import React from 'react';
-
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import cn from 'classnames'
+import React, { useEffect, useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 import logo from '../../images/Logo.svg';
-
-import './style.css';
+import style from './Header.module.scss';
+import logoutIcon from '../../images/logout.icon.svg'
 
 const Header = () => {
+    const [authorized, setAuthorized] = useState(false)
+    const navigate = useNavigate()
+
+    const logout = () => {
+        localStorage.removeItem('token')
+        navigate('/admin/sign-in')
+    }
+
+    useEffect(() => { if (localStorage.getItem('token')) setAuthorized(true) }, [])
+
     return (
         <AppBar position="static" color="primary">
-            <Toolbar className={'toolbar'}>
-                <div className={'logo'}>
+            <Toolbar className={style.toolbar}>
+                <div className={style.logo}>
                     <img src={logo} alt="logo" />
                     <Typography variant="h5" sx={{ flexGrow: 1 }}>
                         KraudDonate
                     </Typography>
                 </div>
+                <div className={style.menu}>
+                    {authorized ?
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <NavLink to="/admin/request" className={({ isActive }) =>
+                                cn(style.menuItem, { [style.activeLink]: isActive })}>
+                                Open requests
+                            </NavLink>
+                            <NavLink to="/close-request" className={({ isActive }) =>
+                                cn(style.menuItem, { [style.activeLink]: isActive })}>
+                                Closed requests
+                            </NavLink>
+                            <Button onClick={logout} sx={{ padding: '0', margin: '0 -10px' }}>
+                                <img src={logoutIcon} className={style.logoutIcon} />
+                            </Button>
+                        </div>
+                        :
+                        <Button
+                            variant="outlined"
+                            size="small"
+                            onClick={() => navigate('/admin/sign-in')}
+                            sx={{ color: '#fff', textTransform: 'capitalize' }}
+                        >
+                            Sing in
+                        </Button>
+                    }
+                </div>
             </Toolbar>
-        </AppBar>
+        </AppBar >
     );
 };
 
